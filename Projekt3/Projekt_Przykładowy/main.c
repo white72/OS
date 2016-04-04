30d3a95ef21d725f6d7d70bf1bd8877ff517b7dc
Jakub Biały <jakubbialy22@gmail.com>
2016-04-04 21:32:07 +0200

    Update main.c/zadanie.txt

--- Projekt3/Projekt_Przykładowy/main.c
+++ Projekt3/Projekt_Przykładowy/main.c
@@ -1,18 +1,10 @@
 #include <unistd.h>
 #include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 int main(){
-	pid_t pid;
-	int fd[2];
-
-	pid = fork();
-
-	if (!pid){
-		puts("Jestem potomkiem");
-	} else {
-		wait(NULL);
-		puts("Nie jestem potomkiem");
-	}
 
 	return 0;
 }
